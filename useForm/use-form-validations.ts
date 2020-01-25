51b188f31d49aaeb137c46f2e42f7ed5668eba31
mery <meryscarlet15@gmail.com>
2020-01-25 12:59:06 +0100

    fix required validation (for checkbox)

--- useForm/use-form-validations.ts
+++ useForm/use-form-validations.ts
@@ -1,4 +1,4 @@
-import { Validator } from 'class-validator'
+import { Validator } from "class-validator";
 import {
   IValidation,
   IDefaultValidation,
@@ -14,74 +14,63 @@ import {
   IErrorTextValidation,
   IEqualsParams,
   IPhoneParams,
-  IMobileParams
-} from './use-form-types'
+  IMobileParams,
+} from "./use-form-types";
 
-const validations = new Validator()
+const validations = new Validator();
 
-export const RequiredValidation: IRequired = <IValue = string | boolean>(
-  params: IErrorTextValidation
-) => {
+export const RequiredValidation: IRequired = <IValue = string | boolean>(params: IErrorTextValidation) => {
   return {
-    tag: 'REQUIRED',
-    method: (value: IValue, name: string) => validations.isEmpty(value),
-    errorText: params.errorText || 'Required field'
-  }
-}
+    tag: "REQUIRED",
+    method: (value: IValue, name: string) => (!value ? false : true),
+    errorText: params.errorText || "Required field",
+  };
+};
 
 export const EmailValidation: IEmail = (params: IErrorTextValidation) => {
   return {
-    tag: 'REQUIRED',
+    tag: "REQUIRED",
     method: (value: string, name: string) => validations.isEmail(value),
-    errorText: params.errorText || 'Invalid email'
-  }
-}
+    errorText: params.errorText || "Invalid email",
+  };
+};
 
-export const EqualsValidation: IEquals = <IValue = string | boolean>(
-  params: IEqualsParams
-) => {
+export const EqualsValidation: IEquals = <IValue = string | boolean>(params: IEqualsParams) => {
   return {
-    tag: 'REQUIRED',
-    method: (value: IValue, name: string, formValues: any) =>
-      formValues[params.compareName] === value,
-    errorText:
-      params.errorText ||
-      `The field is not equal with the ${params.compareName} field`
-  }
-}
+    tag: "REQUIRED",
+    method: (value: IValue, name: string, formValues: any) => formValues[params.compareName] === value,
+    errorText: params.errorText || `The field is not equal with the ${params.compareName} field`,
+  };
+};
 
 export const UrlValidation: IUrl = (params: IErrorTextValidation) => {
   return {
-    tag: 'REQUIRED',
+    tag: "REQUIRED",
     method: (value: string, name: string) => validations.isEmail(value),
-    errorText: params.errorText || 'Invalid url'
-  }
-}
+    errorText: params.errorText || "Invalid url",
+  };
+};
 
-export const CreditCardValidation: ICreditCard = (
-  params: IErrorTextValidation
-) => {
+export const CreditCardValidation: ICreditCard = (params: IErrorTextValidation) => {
   return {
-    tag: 'REQUIRED',
+    tag: "REQUIRED",
     method: (value: string, name: string) => validations.isCreditCard(value),
-    errorText: params.errorText || 'Invalid credit card'
-  }
-}
+    errorText: params.errorText || "Invalid credit card",
+  };
+};
 
 export const PhoneValidation: IPhone = (params: IPhoneParams) => {
   return {
-    tag: 'REQUIRED',
-    method: (value: string, name: string) =>
-      validations.isPhoneNumber(value, params.region),
-    errorText: params.errorText || 'Invalid phone'
-  }
-}
+    tag: "REQUIRED",
+    method: (value: string, name: string) => validations.isPhoneNumber(value, params.region),
+    errorText: params.errorText || "Invalid phone",
+  };
+};
 
 export const MobileValidation: IMobile = (params: IMobileParams) => {
   return {
-    tag: 'REQUIRED',
-    method: (value: string, name: string) =>
-      validations.isPhoneNumber(value, params.region),
-    errorText: params.errorText || 'Invalid phone'
-  }
-}
+    tag: "REQUIRED",
+    method: (value: string, name: string) => validations.isPhoneNumber(value, params.region),
+    errorText: params.errorText || "Invalid phone",
+  };
+};
