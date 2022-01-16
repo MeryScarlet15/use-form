b9074cc2a40037af5d7def5bc9791180d695e635
MeryScarlet15 <meryscarlet15@gmail.com>
2022-01-17 00:14:42 +0100

    Modal success

--- example/src/pages/handle-values/handle-values.tsx
+++ example/src/pages/handle-values/handle-values.tsx
@@ -1,10 +1,70 @@
-import React from 'react'
+import React, { useState } from 'react'
+import ModalSuccess from '../../components/modals/modal-success/modal-success'
 import BodyText from '../../components/text/body-text/body-text'
 import Heading4 from '../../components/text/heading4/heading4'
 import HandleValuesForm from '../../containers/handle-values-form/handle-values-form'
+import useForm from '../../hooks/useForm/use-form'
+import { IFormValidations, ISubmitUseFormParams, IValidation } from '../../hooks/useForm/use-form-types'
+import { EmailValidation, RequiredValidation } from '../../hooks/useForm/use-form-validations'
 import HandleValuesPageContainer from './handle-values-style'
 
+interface IFormValues {
+  name: string;
+  surname: string;
+  email: string;
+}
+
+const requiredValidation: IValidation = RequiredValidation();
+const emailValidation: IValidation = EmailValidation()
+
+const formValidations: IFormValidations<IFormValues> = {
+  name: [requiredValidation],
+  surname: [],
+  email: [requiredValidation, emailValidation]
+}
+
+const useHandleValuesForm = () => {
+  const [submitModal, setSubmitModal] = useState<boolean>(false)
+
+  const initialValues: IFormValues = {
+    name: '',
+    surname: '',
+    email: '',
+  }
+
+  const submit = (params: ISubmitUseFormParams<IFormValues>) => {
+    console.log(params.values)
+    setSubmitModal(true);
+  }
+
+  const formActions = useForm<IFormValues>(
+    {
+      initialValues,
+      formValidations,
+      submit
+    }
+  )
+
+  const handleFillButton = () => {
+    const newValues = {
+      surname: 'Example',
+      email: "example@example.com"
+    }
+
+    formActions.setAGroupOfValues(newValues)
+  }
+
+  return {
+    ...formActions,
+    handleFillButton,
+    submitModal,
+    setSubmitModal
+  }
+}
+
 const HandleValuesPage: React.FC = () => {
+  const actions = useHandleValuesForm()
+
   return <HandleValuesPageContainer>
     <div className="handle-values-wrapper">
       <Heading4 className="handle-values-title">
@@ -18,8 +78,22 @@ const HandleValuesPage: React.FC = () => {
       </BodyText>
 
       <div className="handle-values-form-container">
-        <HandleValuesForm />
+        <HandleValuesForm
+          formActions={actions}
+        />
       </div>
+
+      {
+        actions.submitModal &&
+        <ModalSuccess
+          onClose={() => actions.setSubmitModal(false)}
+          open={actions.submitModal}
+          title="Submit Form"
+          description={`Values: ${actions.values.name} ${actions.values.surname} and ${actions.values.email}`}
+          className="handle-values-submit"
+          headingType="h2"
+        />
+      }
     </div>
   </HandleValuesPageContainer>
 }
