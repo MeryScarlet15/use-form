2b45c70d973e64e31fab76af3c0a4754cd8e322d
MeryScarlet15 <meryscarlet15@gmail.com>
2022-01-16 12:43:41 +0100

    feat main-header responsive

--- example/src/components/main-header/main-header.tsx
+++ example/src/components/main-header/main-header.tsx
@@ -1,14 +1,32 @@
 import React, { useContext } from 'react'
+import { Link } from 'react-router-dom';
+import { routes } from '../../constants/routes';
+import { screenSizes } from '../../constants/screens';
+import { useDetectPlatform } from '../../hooks/useDetectDesktop/use-detect-platform';
 import Heading3 from '../text/heading3/heading3';
+import Heading4 from '../text/heading4/heading4';
 import MainHeaderContainer from './main-header-style';
 
 
 
 const MainHeader: React.FC = () => {
+  const { platform } = useDetectPlatform({ maxMobile: screenSizes.ipad });
+
   return <MainHeaderContainer>
-    <Heading3 className="main-header-logo">
-      <p>Use Form</p>
-    </Heading3>
+    {
+      platform === "mobile" ?
+        <Heading4 className="main-header-logo">
+          <Link to={routes.home}>
+            <p>Use Form</p>
+          </Link>
+        </Heading4>
+        :
+        <Heading3 className="main-header-logo">
+          <Link to={routes.home}>
+            <p>Use Form</p>
+          </Link>
+        </Heading3>
+    }
   </MainHeaderContainer>
 }
 
