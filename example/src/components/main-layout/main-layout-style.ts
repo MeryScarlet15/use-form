3105aa0183bc77488fabb89bbe7fe7a897316922
MeryScarlet15 <meryscarlet15@gmail.com>
2022-01-16 12:45:23 +0100

    feat responsive main-layout

--- example/src/components/main-layout/main-layout-style.ts
+++ example/src/components/main-layout/main-layout-style.ts
@@ -1,9 +1,14 @@
 import styled from 'styled-components'
+import { screenSizes } from '../../constants/screens'
 
 const MainLayoutContainer = styled.div`
   .main-content {
     width: 100%;
-    padding-top: 80px;
+    padding-top: 56px;
+
+    @media screen and (min-width: ${screenSizes.ipad}px){ 
+      padding-top: 80px;
+    }
   }
 `
 
