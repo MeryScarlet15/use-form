f47c9cd5bd6c706c5a7b95cbd9d5d296e69692d8
MeryScarlet15 <meryscarlet15@gmail.com>
2022-01-16 13:43:52 +0100

    upload build

---
+++ example/build/precache-manifest.7b55614e233f071cd1641ba8a058041e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0b5c06a5fb0275627d19c6852a35ba2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd2ea009704943c7b081",
+    "url": "/static/css/main.29117f91.chunk.css"
+  },
+  {
+    "revision": "3ac55f49b0875c329560",
+    "url": "/static/js/2.ee38a9af.chunk.js"
+  },
+  {
+    "revision": "c5fd3f3531adf08c1fa6d5ecddb0ed31",
+    "url": "/static/js/2.ee38a9af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd2ea009704943c7b081",
+    "url": "/static/js/main.8551832f.chunk.js"
+  },
+  {
+    "revision": "8270251058059eb4bd18",
+    "url": "/static/js/runtime-main.38fb33ac.js"
+  }
+]);
